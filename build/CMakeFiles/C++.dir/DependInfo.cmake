486a5739efaabad3b8c78c28dc1db7023fb02ad8
AYUSH RANJAN <ayush.r2345@gmail.com>
2024-05-20 23:06:15 +0530

    Added CMakeLists.txt

---
+++ build/CMakeFiles/C++.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ayushr2345/code/CPP/C++/Basics.cpp" "CMakeFiles/C++.dir/C++/Basics.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/Basics.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/CPPStrings.cpp" "CMakeFiles/C++.dir/C++/CPPStrings.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/CPPStrings.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/CStyleStrings.cpp" "CMakeFiles/C++.dir/C++/CStyleStrings.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/CStyleStrings.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/Functions.cpp" "CMakeFiles/C++.dir/C++/Functions.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/Functions.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/IOStreams.cpp" "CMakeFiles/C++.dir/C++/IOStreams.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/IOStreams.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/Object Oriented Programming/Car.cpp" "CMakeFiles/C++.dir/C++/Object_Oriented_Programming/Car.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/Object_Oriented_Programming/Car.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/Object Oriented Programming/ClassesAndObjects.cpp" "CMakeFiles/C++.dir/C++/Object_Oriented_Programming/ClassesAndObjects.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/Object_Oriented_Programming/ClassesAndObjects.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/Object Oriented Programming/Complex.cpp" "CMakeFiles/C++.dir/C++/Object_Oriented_Programming/Complex.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/Object_Oriented_Programming/Complex.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/Object Oriented Programming/Cuboid.cpp" "CMakeFiles/C++.dir/C++/Object_Oriented_Programming/Cuboid.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/Object_Oriented_Programming/Cuboid.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/Object Oriented Programming/Rectangle.cpp" "CMakeFiles/C++.dir/C++/Object_Oriented_Programming/Rectangle.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/Object_Oriented_Programming/Rectangle.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/PointersAndReferences.cpp" "CMakeFiles/C++.dir/C++/PointersAndReferences.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/PointersAndReferences.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/Student.cpp" "CMakeFiles/C++.dir/C++/Student.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/Student.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/handlers.cpp" "CMakeFiles/C++.dir/C++/handlers.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/handlers.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/main.cpp" "CMakeFiles/C++.dir/C++/main.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/main.cpp.o.d"
+  "/home/ayushr2345/code/CPP/C++/utils.cpp" "CMakeFiles/C++.dir/C++/utils.cpp.o" "gcc" "CMakeFiles/C++.dir/C++/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
